a1bcd5c95959e17a082c1c45e6be9a8cd6e06702
Mike Ryb <myhou@mail.ru>
2025-04-01 22:34:05 +0300

    cardAddFix

--- scripts/index.js
+++ scripts/index.js
@@ -12,16 +12,18 @@ const container = document.querySelector(".places__list");
 function addCard(name, link, del) {
   const cardTemplate = document.querySelector("#card-template").content;
   const card = cardTemplate.querySelector(".card").cloneNode(true);
-  card.querySelector(".card__image").setAttribute("src", link);
+  const image = card.querySelector(".card__image");
+  image.setAttribute("src", link);
+  image.setAttribute("alt", `${name}. Пейзаж.`);
   card.querySelector(".card__title").textContent = name;
-  container.append(card);
-
-  card.querySelector(".card__delete-button").addEventListener("click", del);
+  card.querySelector(".card__delete-button").addEventListener("click",() => del(card));
+  return card;
 }
+
 function deleteCard(e) {
-  e.target.closest(".card").remove();
+  e.remove();
 }
 
 initialCards.forEach(function (loadContent) {
-  addCard(loadContent.name, loadContent.link, deleteCard);
+  container.append(addCard(loadContent.name, loadContent.link, deleteCard));
 });
