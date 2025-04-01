c961c5120c6bca4f666e574a22115a13a5642244
Mike Ryb <myhou@mail.ru>
2025-04-01 20:33:12 +0300

    cards

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,21 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+const container = document.querySelector(".places__list");
+
+function addCard(name, link, del) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const card = cardTemplate.querySelector(".card").cloneNode(true);
+  card.querySelector(".card__image").setAttribute("src", link);
+  card.querySelector(".card__title").textContent = name;
+  container.append(card);
+
+  card.querySelector(".card__delete-button").addEventListener("click", del);
+}
+function deleteCard(e) {
+  e.target.closest(".card").remove();
+}
+
+initialCards.forEach(function (loadContent) {
+  addCard(loadContent.name, loadContent.link, deleteCard);
+});
